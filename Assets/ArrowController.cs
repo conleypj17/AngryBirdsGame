acf978c2e52cb4f3c5654c5c68df32c0daa71359
Patrick Conley <conleypj17@gmail.com>
2025-04-12 18:20:08 -0400

    Arrows are visible but not working as intended

--- Assets/ArrowController.cs
+++ Assets/ArrowController.cs
@@ -2,68 +2,75 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    [SerializeField] GameObject arrowPrefab;   // The arrow symbol (>)
-    [SerializeField] float timeStep = 0.1f;    // Time step for each arrow
-    [SerializeField] int numberOfArrows = 30;  // Number of arrows to show along the path
-    [SerializeField] float gravity = 9.81f;    // Gravity value
+    [SerializeField] GameObject arrowPrefab;
+    [SerializeField] float timeStep = 0.1f;
+    [SerializeField] int numberOfArrows = 30;
+    [SerializeField] float gravity = 9.81f;
 
-    Rigidbody2D birdRigidbody; // Reference to the bird's rigidbody
-    GameObject[] arrows;       // Array to store the arrows
+    Rigidbody2D birdRigidbody;
+    RedBirdController birdController;
+    GameObject[] arrows;
 
     void Start()
     {
-        birdRigidbody = FindObjectOfType<RedBirdController>().GetComponent<Rigidbody2D>();
-        arrows = new GameObject[numberOfArrows];  // Create an array for arrows
+        birdController = FindObjectOfType<RedBirdController>();
+        birdRigidbody = birdController.GetComponent<Rigidbody2D>();
+        arrows = new GameObject[numberOfArrows];
 
-        // Instantiate all arrows at the start, but disable them for now
+        // Instantiate arrows at the start, but deactivate them
         for (int i = 0; i < numberOfArrows; i++)
         {
             arrows[i] = Instantiate(arrowPrefab);
-            arrows[i].SetActive(false); // Hide arrows until needed
+            arrows[i].SetActive(false);
         }
     }
 
     void Update()
     {
-        if (birdRigidbody.isKinematic)  // Only show arrows while dragging the bird
+        if (birdRigidbody.isKinematic) // Only show arrows while the bird is being dragged
         {
             ShowTrajectory();
         }
         else
         {
-            HideArrows();  // Hide arrows once the bird is launched
+            HideArrows(); // Hide arrows once the bird is launched
         }
     }
 
     void ShowTrajectory()
     {
         Vector2 startPosition = birdRigidbody.position;
-        Vector2 launchDirection = FindObjectOfType<RedBirdController>().GetLaunchDirection();
-        float launchPower = FindObjectOfType<RedBirdController>().GetLaunchPower();
-        Vector2 velocity = launchDirection * launchPower;
+        Vector2 launchDirection = birdController.GetLaunchDirection(); // Get the launch direction
+        float launchPower = birdController.GetLaunchPower(); // Get the launch power
+        Vector2 velocity = launchDirection * launchPower; // Calculate the velocity
 
         Vector2 previousPoint = startPosition;
 
         for (int i = 0; i < numberOfArrows; i++)
         {
-            float time = i * timeStep;
-            Vector2 position = startPosition + velocity * time + 0.5f * new Vector2(0, -gravity) * time * time;
+            float time = i * timeStep; // Time step for each arrow
+            Vector2 position = startPosition + velocity * time + 0.5f * new Vector2(0, -gravity) * time * time; // Calculate position
 
-            arrows[i].transform.position = position;
+            arrows[i].transform.position = position; // Set position of the arrow
 
+            // Calculate the direction of the trajectory for this arrow
             Vector2 direction = position - previousPoint;
+
+            // Calculate the angle of the arrow relative to the horizontal axis
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // Apply the correct rotation to the arrow
             arrows[i].transform.rotation = Quaternion.Euler(0, 0, angle);
 
-            arrows[i].SetActive(true);
+            arrows[i].SetActive(true); // Activate the arrow
 
-            previousPoint = position;
+            previousPoint = position; // Update previous point for next arrow
         }
     }
 
     void HideArrows()
     {
-        // Disable all arrows once the bird is launched
+        // Hide all arrows once the bird is launched
         for (int i = 0; i < arrows.Length; i++)
         {
             arrows[i].SetActive(false);
